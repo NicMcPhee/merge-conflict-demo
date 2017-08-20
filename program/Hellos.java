624912345b87997408dda31a1f49f072d5d18cf6
Nic McPhee <mcphee@morris.umn.edu>
2017-08-19 20:16:25 -0500

    Add starter stub for Java program to illustrate conflicts

---
+++ program/Hellos.java
@@ -0,0 +1,13 @@
+/**
+ * A simple Java program used to demonstrate merge conflicts when
+ * multiple people edit the same piece of code.
+ */
+public class Hellos {
+    
+    public static void main(String[] args) {
+	StringBuilder builder = new StringBuilder();
+
+	System.out.println(builder.toString());
+    }
+
+}
