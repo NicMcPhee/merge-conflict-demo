a77b6c24c7c104e8b8322e8cc0c60b1f4b201f2f
Nic McPhee <mcphee@morris.umn.edu>
2017-08-19 21:02:00 -0500

    Add a howdy from Nic
    This adds a hello message from Nic.

--- program/Hellos.java
+++ program/Hellos.java
@@ -3,13 +3,18 @@
  * multiple people edit the same piece of code.
  */
 public class Hellos {
-    
-    public static void main(String[] args) {
-	StringBuilder builder = new StringBuilder();
 
-	builder.append("Hello, folks!\n");
+	public static void main(String[] args) {
+		StringBuilder builder = new StringBuilder();
 
-	System.out.println(builder.toString());
-    }
+		builder.append("Hello, folks!\n");
+		builder.append(nic_says_hello());
+
+		System.out.println(builder.toString());
+	}
+
+	public static String nic_says_hello() {
+		return "Nic says 'Howdy!'\n";
+	}
 
 }
